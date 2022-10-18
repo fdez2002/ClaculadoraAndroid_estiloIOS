6b90599e926596ac04ea2f4d69e756164228df55
antonio <fdez2002@gmail.com>
2022-10-18 18:03:16 +0200

    Cambio formato boton 0

--- app/src/main/java/com/example/claculadoraandroid/MainActivity.java
+++ app/src/main/java/com/example/claculadoraandroid/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.claculadoraandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView operaciones;
+    private Button ceroBoton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operaciones = findViewById(R.id.textViewOperaciones);
+        ceroBoton = findViewById(R.id.button0);
+
+
+
+
+
     }
+    private void numeroPresionado(String digito){
+
+        operaciones.setText(digito);
+    }
+
+
 }
